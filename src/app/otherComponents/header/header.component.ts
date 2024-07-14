3fef427df2720e70cc916a7be9726d91af4ee2f7
Amit Niranjan <amitniranjan9@gmail.com>
2024-07-15 01:32:06 +0530

    Menu Added

--- src/app/otherComponents/header/header.component.ts
+++ src/app/otherComponents/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, TemplateRef } from '@angular/core';
+import { NgbOffcanvas, NgbOffcanvasConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  providers: [NgbOffcanvasConfig, NgbOffcanvas],
+
 })
 export class HeaderComponent {
+  constructor(
+		config: NgbOffcanvasConfig,
+		private offcanvasService: NgbOffcanvas,
+	) {
+		// customize default values of offcanvas used by this component tree
+		config.position = 'end';
+		config.backdropClass = 'bg-info';
+		config.keyboard = false;
+	}
 
+	open(content: TemplateRef<any>) {
+		this.offcanvasService.open(content);
+	}
 }
