c2f6389b1d755ced16a519d0e5b52894e41a5408
Amit Niranjan <amitniranjan9@gmail.com>
2024-09-23 01:07:43 +0530

    Service page done

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef } from '@angular/core';
+import { NgbCarouselModule, ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+  private modalService = inject(NgbModal);
+	closeResult = '';
 
+  open(content: TemplateRef<any>) {
+    this.modalService.open(content, { centered: true, windowClass: 'services-dialog' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+	private getDismissReason(reason: any): string {
+		switch (reason) {
+			case ModalDismissReasons.ESC:
+				return 'by pressing ESC';
+			case ModalDismissReasons.BACKDROP_CLICK:
+				return 'by clicking on a backdrop';
+			default:
+				return `with: ${reason}`;
+		}
+	}
 }
